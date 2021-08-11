032d7e65676f2518881940adc88c899fe31d5922
wbadmin <mikhail.nogovitsyn2015@yandex.ru>
2021-08-11 15:21:47 +0300

    All servlet classes created

--- src/main/java/soa/labwork/servlets/AddElementServlet.java
+++ src/main/java/soa/labwork/servlets/AddElementServlet.java
@@ -1,4 +1,24 @@
 package soa.labwork.servlets;
 
-public class AddElementServlet {
+import soa.labwork.entities.Organization;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/add")
+public class AddElementServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/xml");
+        PrintWriter printWriter = resp.getWriter();
+        //printWriter.write(Organization.toString());
+        printWriter.write("<a id=\"a\"><b id=\"b\">hey!</b></a>");
+        printWriter.close();
+    }
 }
